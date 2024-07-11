0f5c3876ab03f23d50fad596d86931d2de73a3f0
Facundo <faccu.veron92@gmail.com>
2024-07-11 10:24:35 -0300

    un monton de cambios

--- shadcn-dashboard/src/app/dashboard/alert/page.tsx
+++ shadcn-dashboard/src/app/dashboard/alert/page.tsx
@@ -1,7 +1,33 @@
+import { Alert, AlertTitle, AlertDescription} from "@/components/ui/alert";
+import { Button } from "@/components/ui/button";
+import { RocketIcon } from "lucide-react";
+import Link from "next/link";
+
 export default function Page(){
-    return(
-        <div>
-            Alert
-        </div>
-    )
+    return <>
+           
+
+            <div className="gird gap-3">
+               
+               <Alert variant={"destructive"}>
+                    <RocketIcon className="h-4 w-4" />
+                    <AlertTitle>Heads up!</AlertTitle>
+                    <AlertDescription>
+                        You can add components to your app using the cli.
+                    </AlertDescription>
+                </Alert>
+
+                <Alert variant={"success"}>
+                    <RocketIcon className="h-4 w-4" />
+                    <AlertTitle>Heads up!</AlertTitle>
+                    <AlertDescription>
+                        You can add components to your app using the cli.
+                    </AlertDescription>
+                </Alert>
+              
+               
+            </div>
+    </>
+        
+    
 }
